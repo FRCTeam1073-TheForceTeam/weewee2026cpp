a5e1d442476c2124bee39c2a2b9308b7156566b3
roboticsgeek <rtpack@charter.net>
2025-12-10 20:50:39 -0500

    Added a udp class and std::array.

--- src/main/cpp/subsystems/AprilTag.cpp
+++ src/main/cpp/subsystems/AprilTag.cpp
@@ -1,12 +1,20 @@
 #include "subsystems/AprilTag.h"
 
-AprilTag::AprilTag() {
+AprilTag::AprilTag() : m_udp_client("0.0.0.0:5800", m_logger) {
+
   // Implementation of subsystem constructor goes here.
+  m_udp_client.start();
+}
+
+AprilTag::~AprilTag() {
+    m_udp_client.shutdown();
 }
 
 
 void AprilTag::Periodic() {
   // Implementation of subsystem periodic method goes here.
+  // TODO:
+  auto result = m_udp_client.receive(m_packet_buffer.data(), m_packet_buffer.size());
 }
 
 void AprilTag::SimulationPeriodic() {
--- src/main/include/subsystems/AprilTag.h
+++ src/main/include/subsystems/AprilTag.h
@@ -4,10 +4,15 @@
 
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
+#include <wpinet/UDPClient.h>
+#include <wpi/Logger.h>
+#include <array>
 
 class AprilTag : public frc2::SubsystemBase {
  public:
   AprilTag();
+
+  ~AprilTag();
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -21,6 +26,7 @@ class AprilTag : public frc2::SubsystemBase {
   void SimulationPeriodic() override;
 
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  wpi::Logger m_logger;
+  wpi::UDPClient m_udp_client;
+  std::array<uint8_t, 1024> m_packet_buffer;
 };
