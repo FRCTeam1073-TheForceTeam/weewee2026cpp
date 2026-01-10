c8145187a32dfcb1f96ac3971f85d543e6f761fc
William Van Uitert <william.vanuitert@gmail.com>
2026-01-10 13:07:50 -0500

    pushing stuff

--- src/main/cpp/subsystems/OI.cpp
+++ src/main/cpp/subsystems/OI.cpp
@@ -3,15 +3,52 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/OI.h"
+#include <iostream>
 
 OI::OI() :
-    _driverController = new frc::Joystick(0);
-    _operatorController = new frc::Joystick(1);
-    _driverAButton = new frc2::JoystickButton(_driverController, Buttons::AButton);
-    _driverBButton = new frc2::JoystickButton(_driverController, Buttons::BButton);
-    _driverXButton = new frc2::JoystickButton(_driverController, Buttons::XButton);
-    _driverYButton = new frc2::JoystickButton(_driverController, Buttons::YButton);
+_driverController(0),
+_operatorController(1),
+_hardwareConfigured(false)
+{
+    _hardwareConfigured &= ConfigureHardware();
+
+    if (_hardwareConfigured) {
+        std::cerr << "OI hardware configuration error!!" << std::endl;
+    }
+}
 
 
 // This method will be called once per scheduler run
 void OI::Periodic() {}
+
+double GetDriverLeftX() {
+    return _driverController.GetLeftX();
+}
+
+double GetDriverLeftY() {
+    return _driverController.GetLeftY();
+};
+
+double GetDriverRightX() {
+    return _driverController.GetRightX();
+};
+
+double GetDriverRightY() {
+    return _driverController.GetRightY();
+};
+
+double GetOperatorLeftX() {
+    return _operatorController.GetLeftX();
+};
+
+double GetOperatorLeftY() {
+    return _operatorController.GetLeftY();
+};
+
+double GetOperatorRightX() {
+    return _operatorController.GetRightX();
+};
+
+double GetOperatorRightY() {
+    return _operatorController.GetRightY();
+};
