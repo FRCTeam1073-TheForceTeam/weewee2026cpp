5d0bdfaf7d17c8177e1faa59f15a8f7e53697eef
William <92765324+Mindi088@users.noreply.github.com>
2026-01-22 20:15:22 -0500

    imports were added

--- src/main/cpp/subsystems/ShooterRotate.cpp
+++ src/main/cpp/subsystems/ShooterRotate.cpp
@@ -2,7 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <iostream>
 #include "subsystems/ShooterRotate.h"
+#include <ctre/phoenix6/controls/NeutralOut.hpp>
 
 RotateShooter::RotateShooter() = default;
 
