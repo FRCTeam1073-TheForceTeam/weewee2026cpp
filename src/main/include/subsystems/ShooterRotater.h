1ac1e063990d7d9e717a83208524c241f8f8886a
Allen <allen.g@frc1073.org>
2026-02-10 18:30:09 -0500

    shooter was added  to main

---
+++ src/main/include/subsystems/ShooterRotater.h
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/SubsystemBase.h>
+
+#include <units/length.h>
+#include <units/velocity.h>
+#include <units/force.h>
+
+#include <ctre/phoenix6/TalonFX.hpp>
+
+#include <variant>
+
+
+/**
+ * This example subsystem shows the basic pattern of any mechanism subsystem.
+ * 
+ * If configures some harwdare, provides feedback and accepts (modal) commands
+ * for the subsystem. It handles feedback using signals (efficently),
+ * and provides latency compensated feedabck. Lots of good practices for
+ * a nicely behaved subsystem are included as example code to get started
+ * on other subsystems.
+ * 
+ */
+class ShooterRotater : public frc2::SubsystemBase {
+ public:
+
+  // CANBusID for the motor.
+  static constexpr int RotaterMotorId = 25;
+  static constexpr int encoderMotorId = 26;
+
+  // Mechanism conversion constants for the subsystem:
+  static constexpr auto TurnsPerMeter = units::angle::turn_t(32.0) / units::length::meter_t(1.0);
+  static constexpr auto AmpsPerNewton = units::current::ampere_t(10.0) / units::force::newton_t(1.0);
+
+  
+  // The feedback for this subsystem provided as a struct.
+  struct Feedback {
+      units::length::meter_t position;
+      units::force::newton_t force;
+  };
+
+
+  // Commands may be modal (different command modes):
+  // std::monostate is the "empty" command or "no command given".
+  // Otherwise you can have two different types of commands.
+  using Command = std::variant<std::monostate, units::velocity::meters_per_second_t, units::length::meter_t>;
+
+
+  // Constructor for the subsystem.
+  ShooterRotater();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   * 
+   * This function samples and updates feedback from hardware, and sends target
+   * command information to the hardware.
+   */
+  void Periodic() override;
+
+
+  /// Access the latest feedback from the system. 
+  const Feedback& GetFeedback() const { return _feedback; }
+
+  /// Set the command for the system.
+  void SetCommand(Command cmd);
+  units::angle::radian_t GetAngle();
+  void SetTargetAngle(units::angle::radian_t newAngle);
+  units::angular_velocity::radians_per_second_t GetVelocity();
+  void SetTargetVelocity(units::angular_velocity::radians_per_second_t vel);
+
+
+ private:
+
+
+  // Helper function for configuring hardware from within the constructor of the subsystem.
+  bool ConfigureHardware();
+
+  // Did we successfully configure the hardware?
+  bool _hardwareConfigured;
+
+  // Example TalonFX motor interface.
+  ctre::phoenix6::hardware::TalonFX _rotaterMotor;
+
+  units::angle::radian_t targetAngle;
+  units::angle::radian_t positionAngle;
+
+  units::angular_velocity::radians_per_second_t velocity;
+  units::angular_velocity::radians_per_second_t targetVelocity;
+
+  // CTRE hardware feedback signals:
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> _rotaterPositionSig;
+  ctre::phoenix6::StatusSignal<units::current::ampere_t> _rotaterCurrentSig;
+
+
+  // Example velocity and position controls:
+  ctre::phoenix6::controls::PositionVoltage _commandPositionVoltage;  // Uses Slot0 gains.
+  
+  // Cached feedback:
+  Feedback _feedback;
+
+  // Cached command: Variant of possible different kinds of commands.
+  Command  _command;
+
+};
